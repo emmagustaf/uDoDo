e91cbe5c199818a071c6d12aa25b298ec93c5567
terrorulf <terrorulf@github.com>
2013-04-15 11:12:36 +0200

    added enum for Priority, implementation for changePriority() and implemenation for a simple constructor samt lite annat bös

--- src/Model/Task.java
+++ src/Model/Task.java
@@ -1,19 +1,36 @@
 package Model;
 
 public class Task {
+	private enum Priority{
+		HIGH_PRIORITY, NO_PRIORITY
+	}
 	private String title;
+	private String description;
+	private Priority prio;
 	
-	public Task(){
-		
+	public Task(String title){
+		this.title = title;
+		prio = Priority.NO_PRIORITY;
 	}
 	public void removeTask(){
 		
 	}
 	public void changePriority(){
-		
+		switch (prio) {
+			case NO_PRIORITY:
+				prio = Priority.HIGH_PRIORITY;
+				break;
+			case HIGH_PRIORITY:
+				prio = Priority.NO_PRIORITY;
+				break;
+			default:
+				prio = this.getPrio();
+				break;
+		}
+			
 	}
 	public Priority getPrio(){
-		return priority;
+		return prio;
 	}
 	public String getTitle(){
 		return title;
