7e03647890293705492b1c19e85f406661832b6b
terrorulf <terrorulf@github.com>
2013-04-16 14:03:41 +0200

    merged

--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -24,24 +24,12 @@ public class GUIView extends JPanel {
 		headerPanel.add(header);
 		listPanel.add(list);
 		setLayout(new BorderLayout(0, 0));
-<<<<<<< HEAD
-		add(header, BorderLayout.NORTH);
-		
-		JPanel taskPresenter = new JPanel();
-		TaskPanel panel = new TaskPanel(null);
-		add(taskPresenter, BorderLayout.CENTER);
-		taskPresenter.add(panel);
-		this.pack();
-		repaint();
-=======
+
 		add(headerPanel, BorderLayout.NORTH);
 		add(listPanel, BorderLayout.CENTER);
 				
 		setVisible(true);
->>>>>>> 8f70487c55a96f3fc3ffe6157a3bcd9de7e07959
 
-		//TODO create panels to add panels in?
-		//TODO instances from the classes HeaderView and ListView should be added to the GUI. 
-	}
+		}
 	
 }
