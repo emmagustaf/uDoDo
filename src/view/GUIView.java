503646de1b9c6970efdb74f1ea104df6a6ffd6c6
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-04-16 11:20:22 +0200

    Made a few changes in GUIView and ListView

--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -13,11 +13,18 @@ public class GUIView extends JPanel {
 	private ListView list;
 
 	/**
-	 * Create the frame.
+	 * Creates the frame.
 	 */
 	public GUIView(HeaderView header, ListView list) {
 		this.header = header;
 		this.list = list;
+		setLayout(new BorderLayout(0, 0));
+		add(header, BorderLayout.NORTH);
+		add(list, BorderLayout.CENTER);
+		TaskPanel panel = new TaskPanel(null);
+	
+		list.add(panel);
+		repaint();
 
 		//TODO create panels to add panels in?
 		//TODO instances from the classes HeaderView and ListView should be added to the GUI. 
