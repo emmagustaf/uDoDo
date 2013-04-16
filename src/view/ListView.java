7995605e90d2fae6728447b8ace61e9145d86baf
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-04-16 14:49:26 +0200

    bööös

--- src/view/ListView.java
+++ src/view/ListView.java
@@ -2,7 +2,6 @@ package view;
 
 import javax.swing.JPanel;
 
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 public class ListView extends JPanel {
@@ -11,7 +10,7 @@ public class ListView extends JPanel {
 	 * Create the panel.
 	 */
 	public ListView() {
-		setLayout(new GridBagLayout());
+		setLayout(new GridLayout(1, 1000));
 		
 		//TODO Really nothing to do here.. Some sort of grid perhaps so the TaskPanels are added in a vertical row.. 
 
