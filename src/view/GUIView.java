4b082bebd4c2e426cc9643db48a0539874dd2f12
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-04-16 14:12:00 +0200

    small changes

--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -1,16 +1,11 @@
 package view;
-
 import java.awt.*;
 import javax.swing.*;
-
 public class GUIView extends JPanel {
-
 	private HeaderView header;
 	private ListView list;
 	private JPanel headerPanel = new JPanel();
 	private JPanel listPanel = new JPanel();
-	
-	
 	/**
 	 * Creates the frame.
 	 */
@@ -18,13 +13,10 @@ public class GUIView extends JPanel {
 		this.header = header;
 		this.list = list;
 		headerPanel.add(header);
-		listPanel.add(list);
+		listPanel.add(list);	
 		setLayout(new BorderLayout(0, 0));
 		add(headerPanel, BorderLayout.NORTH);
 		add(listPanel, BorderLayout.CENTER);
-				
 		setVisible(true);
-
-	}
-	
+	}	
 }
