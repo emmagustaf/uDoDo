08e257342290ab6dce81dab1cee5edcfeed87f53
Josefin Ondrus <josefin.ondrus@gmail.com>
2013-04-16 13:47:51 +0200

    Making methods in controller public.

--- src/controller/ListController.java
+++ src/controller/ListController.java
@@ -10,19 +10,19 @@ public class ListController {
 		this.view = view;
 	}
 	
-	private void addTask(TaskPanel task){
+	public void addTask(TaskPanel task){
 		//TODO add task to the listview.
 		view.add(task);
 		view.repaint();
 		
 	}
 	
-	private void removeTask(TaskPanel task){
+	public void removeTask(TaskPanel task){
 		//TODO remove task from the listview.
 		view.remove(task);
 	}
 	
-	private void updateListView(){
+	public void updateListView(){
 		//TODO update the listview.
 		view.repaint();
 	}
