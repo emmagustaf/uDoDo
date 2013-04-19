f55603137fffea64db6d7fd6ef00dbdb6602ffcb
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-04-19 15:08:48 +0200

    erased a previous error

--- src/view/ListView.java
+++ src/view/ListView.java
@@ -2,8 +2,7 @@ package view;
 
 import javax.swing.JPanel;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
 
 public class ListView extends JPanel {
 	/**
@@ -12,8 +11,8 @@ public class ListView extends JPanel {
 	public ListView() {
 		setLayout(new GridLayout(1000, 1));
 		
-		//TODO Really nothing to do here.. Some sort of grid perhaps so the TaskPanels are added in a vertical row.. 
+		
 
 	}
-
+	
 }
