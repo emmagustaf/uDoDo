0530dd3961f3920ffbc459daa975008c1180f4ed
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-04-30 16:19:29 +0200

    few changes

--- src/Model/CategoryModel.java
+++ src/Model/CategoryModel.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class CategoryModel {
 	
-	private List <TaskModel> list;
+	//private List <TaskModel> list;
 	private String catTitle;
 	
 	public CategoryModel(String catTitle){
 		this.catTitle=catTitle;
-		list=new ArrayList<TaskModel>();
+	//	list=new ArrayList<TaskModel>();
 	
 	}
 	
-	public void addTask(TaskModel task){
+/*	public void addTask(TaskModel task){
 		list.add(task);
 	}
-	
+	*/
 	public String getCatTitle(){
 		return catTitle;
 	}
--- src/view/CategoryView.java
+++ src/view/CategoryView.java
@@ -1,12 +1,19 @@
 package view;
 
 import java.awt.*;
-
+import controller.*;
 import javax.swing.*;
 public class CategoryView extends JPanel {
+	private CategoryController controller;
+	private JTextField categoryTitleInput;
 	
-	public CategoryView(){
-		setLayout(new GridLayout(1000, 1));
+	public CategoryView(CategoryController controller){
+		categoryTitleInput = new JTextField(20);
+		this.controller=controller;
+		JButton addNewCategoryButton = new JButton ("Create new category");
+		this.add(categoryTitleInput);
+		this.add(addNewCategoryButton);
+		addNewCategoryButton.addActionListener(null);
 	}
 
 }
