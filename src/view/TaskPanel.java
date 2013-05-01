e5630b56e53c05f2614bdd0e7b6d91d5128e984c
Josefin Ondrus <josefin.ondrus@gmail.com>
2013-05-01 16:38:26 +0200

    Changes in TaskModel and a new class TaskController

--- src/view/TaskPanel.java
+++ src/view/TaskPanel.java
@@ -8,6 +8,12 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JCheckBox;
+import java.awt.Dimension;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.Component;
 
 /**
  * A class to represent a TaskPanel. 
@@ -18,20 +24,26 @@ import javax.swing.JCheckBox;
 public class TaskPanel extends JPanel {
 	
 	private TaskModel model;
-
+	private String taskCheck;
 	/**
 	 * Create the panel.
 	 */
 	public TaskPanel(TaskModel model) {
+		setBorder(new LineBorder(new Color(0, 0, 0)));
+		setMinimumSize(new Dimension(30, 20));
+		setPreferredSize(new Dimension(470, 70));
+		setSize(new Dimension(30, 20));
 		this.model=model;
-	
-		setLayout(new MigLayout("", "[][][][][][][]", "[]"));
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JLabel taskLabel = new JLabel(model.getTitle());
+		taskLabel.setSize(new Dimension(30, 20));
 		taskLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		add(taskLabel, "cell 1 0");
+		add(taskLabel);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("");
-		add(chckbxNewCheckBox, "cell 6 0");
+		chckbxNewCheckBox.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		chckbxNewCheckBox.setActionCommand(taskCheck);
+		add(chckbxNewCheckBox);
 	}
 }
