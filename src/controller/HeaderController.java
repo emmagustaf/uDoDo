39a3b7fa559d30463a9ccb778b90c9ac516cc159
terrorulf <terrorulf@github.com>
2013-05-02 17:00:13 +0200

    few changes

--- src/controller/HeaderController.java
+++ src/controller/HeaderController.java
@@ -3,11 +3,12 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.text.html.ListView;
+import javax.swing.JButton;
 
 import model.TaskModel;
 
 import view.HeaderView;
+import view.ListView;
 import view.TaskPanel;
 
 public class HeaderController implements ActionListener{
@@ -22,11 +23,12 @@ public class HeaderController implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof HeaderView){
+		if(e.getSource() instanceof JButton){
 			if(e.getActionCommand().equals("newTask") && headerView.getTextField().length() > 0){
 				TaskModel task = new TaskModel(headerView.getTextField());
 				TaskPanel taskPanel = new TaskPanel(task);
-				
+				view.add(taskPanel);
+				view.
 			}
 		}
 	}
