7c1322bf7387fb290e093b188cbdbd71dd54552f
terrorulf <terrorulf@github.com>
2013-05-02 10:32:25 +0200

    changes in headerview and headercontroller

--- src/controller/HeaderController.java
+++ src/controller/HeaderController.java
@@ -6,15 +6,22 @@ import java.awt.event.ActionListener;
 import model.TaskModel;
 
 import view.HeaderView;
+import view.TaskPanel;
 
 public class HeaderController implements ActionListener{
 	
-
+	private HeaderView view;
+	
+	public HeaderController(HeaderView view){
+		this.view = view;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() instanceof HeaderView){
-			if(e.getActionCommand().equals("newTask")){
-				TaskModel task = new TaskModel();
+			if(e.getActionCommand().equals("newTask") && view.getTextField().length() > 0){
+				TaskModel task = new TaskModel(view.getTextField());
+				TaskPanel taskPanel = new TaskPanel(task);
 			}
 		}
 	}
--- src/view/HeaderView.java
+++ src/view/HeaderView.java
@@ -10,36 +10,32 @@ import controller.ListController;
 public class HeaderView extends JPanel{
 
 	//TODO Create button "Ny" and textfield to get taskname
-	private ListController controller;
+	private HeaderController controller;
 	private JTextField taskTitleInput;
 
 	
-	public HeaderView(ListController controller) {
-		this.controller = controller;
+	public HeaderView() {
 		taskTitleInput = new JTextField(50);
 		JButton addNewTaskButton = new JButton("Add");
 		this.add(taskTitleInput);
 		this.add(addNewTaskButton);
-		addNewTaskButton.addActionListener(new HeaderController());
+		controller = new HeaderController(this);
+		addNewTaskButton.addActionListener(controller);
 		addNewTaskButton.setActionCommand("newTask");
 	}
 	
-	public JTextField getTextField(){
-		return taskTitleInput;
+	public String getTextField(){
+		return taskTitleInput.getText();
 	}
-	private class NewTaskListener implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if(e.getSource() instanceof JButton){
-				if(taskTitleInput.getText().length() > 0){
-					TaskModel task = new TaskModel(taskTitleInput.getText());
-					TaskPanel taskPanel = new TaskPanel(task);
-					controller.addTask(taskPanel);
-					controller.updateListView();
-				}
-			}
-		}
+
+	//private class NewTaskListener implements ActionListener{
+		//public void actionPerformed(ActionEvent e){
+				//controller.addTask(taskPanel);
+				//controller.updateListView();
 		
-	}
+		//}
+		
+	//}
 	
 	//TODO Create action event for button "Ny". 
 		//TO DO Should create a new instance of the TaskModel-class (with the string from the textfield).
