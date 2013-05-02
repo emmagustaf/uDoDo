ddc9d11dc6512ca0449bd33ff6d02f4315052688
Josefin Ondrus <josefin.ondrus@gmail.com>
2013-05-02 10:37:35 +0200

    Chamges in list-classes.

--- src/controller/ListController.java
+++ src/controller/ListController.java
@@ -4,21 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import view.*;
+import model.*;
 
 public class ListController implements ActionListener {
 
 	private ListView view;
 	
-	public ListController(ListView view){
+	public ListController(ListView view, ListModel model){
 		this.view = view;
 	}
 	
 	public void addTask(TaskPanel task){
-		//TODO add task to the listview.
 		view.add(task);
 		view.revalidate();
 		view.repaint();
-		
 	}
 	
 	public void removeTask(TaskPanel task){
