ab0a973d179ea6a0b6c2e5b4db76a6928a98c6a2
Josefin Ondrus <josefin.ondrus@gmail.com>
2013-05-05 21:58:05 +0200

    Trying to fix visual layout...

--- src/view/GUIView.java
+++ src/view/GUIView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.*;
+
 import javax.swing.*;
 import utility.*;
 public class GUIView extends JPanel {
@@ -13,6 +14,7 @@ public class GUIView extends JPanel {
 	private JPanel listPanel = new JPanel();
 	private JPanel categoryPanel = new JPanel();
 	private JPanel topPanel = new JPanel();
+	private JPanel displayPanel = new JPanel();
 	
 	/**
 	 * Creates the frame.
@@ -20,40 +22,79 @@ public class GUIView extends JPanel {
 	
 	public GUIView(HeaderView header, ListView list, CategoryView cat, TopView top, CategoryListView listView) {
 		
-		this.header = header;
-		this.list = list;
+		this.header=header;
+		this.list=list;
 		this.cat=cat;
 		this.top=top;
-		this.listView = listView;
+		this.listView=listView;
 		
-		this.setMinimumSize(new Dimension(550, 302));
+		this.setMinimumSize(new Dimension(640, 490));
 		this.setBackground(GraphicConstants.BACKGROUND);
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
-		headerPanel.setMinimumSize(new Dimension(397, 62));
-		headerPanel.setBackground(GraphicConstants.BACKGROUND);
+		//headerPanel.setMinimumSize(new Dimension(200, 62));
+//		headerPanel.setBackground(Color.blue);
+//		headerPanel.setPreferredSize(new Dimension(650, 100));
+//		headerPanel.setMinimumSize(new Dimension(650, 100));
+//		headerPanel.setMaximumSize(new Dimension(650, 100));
 		
-		categoryPanel.setMinimumSize(new Dimension(153, 240));
-		categoryPanel.setBackground(GraphicConstants.BACKGROUND);
+		//categoryPanel.setMinimumSize(new Dimension(200, 240));
+		categoryPanel.setBackground(Color.yellow);
 		
-		listPanel.setMinimumSize(new Dimension(150, 240));
-		listPanel.setBackground(GraphicConstants.BACKGROUND);
+		//listPanel.setMinimumSize(new Dimension(150, 240));
+		listPanel.setBackground(Color.green);
 		
-		topPanel.setMinimumSize(new Dimension(397, 62));
-		topPanel.setBackground(GraphicConstants.BACKGROUND);
+		//topPanel.setMinimumSize(new Dimension(630, 62));
+		topPanel.setBackground(Color.ORANGE);
 		
 		
-		FlowLayout flowLayout = (FlowLayout) listPanel.getLayout();
+		//FlowLayout flowLayout = (FlowLayout) listPanel.getLayout();
+		topPanel.add(top);
+		//c.ipady = 40;      
+		c.weightx = 0.0;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(topPanel, c);
+		
+		categoryPanel.add(cat);
+		c.weightx = 0.0;
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		add(categoryPanel, c);
+		
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		add(listView, c);
+		
+		listPanel.add(list);
+		c.weightx = 0.0;
+		c.gridwidth = 1;
+		c.gridx = 1;
+		c.gridy = 2;
+		add(listPanel, c);
 		
 		headerPanel.add(header);
-		listPanel.add(list);
-		categoryPanel.add(cat);
+		c.weightx = 0.0;
+		c.gridwidth = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		add(headerPanel, c);
 		
-		topPanel.add(top);
-		setLayout(new BorderLayout());
-		this.add(topPanel, BorderLayout.NORTH);
-		this.add(headerPanel, BorderLayout.CENTER);
-		this.add(listPanel, BorderLayout.SOUTH);
-		this.add(categoryPanel, BorderLayout.WEST);
+		c.gridheight = 2;
+		c.gridx = 2;
+		c.gridy = 1;
+		add(displayPanel, c);
+		//setLayout(new BorderLayout());
+		//this.add(topPanel, BorderLayout.NORTH);
+//		this.add(headerPanel, BorderLayout.CENTER);
+//		this.add(listPanel, BorderLayout.SOUTH);
+//		this.add(categoryPanel, BorderLayout.WEST);
+		revalidate();
+		repaint();
 		this.setVisible(true);
 		
 	}	
--- src/view/TopView.java
+++ src/view/TopView.java
@@ -7,14 +7,20 @@ import javax.swing.JPanel;
 import utility.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
+import net.miginfocom.swing.MigLayout;
 
 public class TopView extends JPanel {
 	
 	public TopView(){
 		
-		setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		setSize(new Dimension(620, 62));
+		this.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		//this.setSize(new Dimension(620, 70));
+		setPreferredSize(new Dimension(600, 70));
+		setMinimumSize(new Dimension(600, 70));
+		setMaximumSize(new Dimension(600, 70));
 		this.setBackground(GraphicConstants.BACKGROUND);
+		setLayout(new MigLayout("", "[]", "[]"));
+		setVisible(true);
 	
 	}
 }
