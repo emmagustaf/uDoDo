9431913a59da7d427760531bc5f9d9faa94b64f9
materne <hanna.materne@live.se>
2013-05-10 15:08:00 +0200

    added things in some controllers

--- src/controller/DisplayController.java
+++ src/controller/DisplayController.java
@@ -2,6 +2,10 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
 
 import javax.swing.JComponent;
 
@@ -14,13 +18,51 @@ public class DisplayController implements ActionListener{
 	private EditView editView;
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof DisplayModel){
-//			DisplayModel model = (DisplayModel) e.getSource();
-			if("editButton".equals(e.getActionCommand())){
+		//if(e.getSource() instanceof DisplayModel){
+  //		DisplayModel model = (DisplayModel) e.getSource();
+			//if("editButton".equals(e.getActionCommand())){
+				//editView.setVisible(true);
+			//}
+		//}
+		if(e.getSource() instanceof JComponent){
+			if(e.getActionCommand.equals("editTask")){
 				editView.setVisible(true);
 			}
-		}
 				
+	}
+	private class MouseMethods implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
 	}
 
 }
