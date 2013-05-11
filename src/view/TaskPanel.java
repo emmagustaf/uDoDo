6acd8264440797da156be2740abe11260332c5e1
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-05-11 22:13:51 +0200

    erased unused layout

--- src/view/TaskPanel.java
+++ src/view/TaskPanel.java
@@ -40,35 +40,19 @@ public class TaskPanel extends JPanel {
 		this.model=model;
 		setLayout(new MigLayout("", "[40.00px,left][100.00][30.00][88.00][93.00]", "[45.00px,center]"));
 		
-//		GridBagLayout gridBagLayout = new GridBagLayout();
-//		gridBagLayout.columnWidths = new int[]{32, 127, 0};
-//		gridBagLayout.rowHeights = new int[]{111, 0};
-//		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0};
-//		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-//		setLayout(gridBagLayout);
-		
-		
-		
 		JToggleButton chckbxNewCheckBox = new JToggleButton("");
 		chckbxNewCheckBox.setIcon(new ImageIcon(TaskPanel.class.getResource("/utility/icons/checkmark_icon&16.png")));
 		chckbxNewCheckBox.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		chckbxNewCheckBox.setActionCommand(taskCheck);
 		add(chckbxNewCheckBox, "cell 0 0,alignx center,aligny center");
-		//GridBagConstraints gbc_chckbx = new GridBagConstraints();
-//		gbc_chckbx.gridx = 0;
-//		gbc_chckbx.gridy = 0;
-//		add(chckbxNewCheckBox, gbc_chckbx);
-//		
+		
 		JLabel taskLabel = new JLabel(model.getTitle());
 		taskLabel.setMinimumSize(new Dimension(200, 30));
 		taskLabel.setMaximumSize(new Dimension(200, 30));
 		taskLabel.setPreferredSize(new Dimension(200, 30));
 		taskLabel.setFont(GraphicConstants.SMALLHEADINGFONT);
 		add(taskLabel, "cell 1 0 3 1,alignx left,aligny center");
-//		GridBagConstraints gbc_taskLabel = new GridBagConstraints();
-//		gbc_taskLabel.gridx =1;
-//		gbc_taskLabel.gridy = 0;
-//		add(taskLabel, gbc_taskLabel);
+
 		
 		deleteTaskButton = new JButton();
 		deleteTaskButton.setOpaque(false);
@@ -77,11 +61,6 @@ public class TaskPanel extends JPanel {
 		deleteTaskButton.setActionCommand("deleteTask");
 		add(deleteTaskButton, "cell 4 0,alignx right,aligny center");
 		
-//		GridBagConstraints gbc_deleteTaskButton = new GridBagConstraints();
-//		gbc_deleteTaskButton.anchor = GridBagConstraints.WEST;
-//		gbc_deleteTaskButton.gridx = 2;
-//		gbc_deleteTaskButton.gridy = 0;
-//		add(deleteTaskButton, gbc_deleteTaskButton);
 	}
 	
 	public void setController(TaskController controller){
