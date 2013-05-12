34a411033b51c55b3177d3fc2435b490fbbb550a
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-05-12 18:28:27 +0200

    changed actioncontroller in categoryview

--- src/controller/DisplayController.java
+++ src/controller/DisplayController.java
@@ -13,82 +13,75 @@ import view.*;
 import model.DisplayModel;
 import model.EditModel;
 
-public class DisplayController implements ActionListener, MouseListener{
+public class DisplayController implements ActionListener, MouseListener {
 	private TaskSettingView settingView;
 	private DisplayTaskPanel displayTaskPanel;
-	//private MouseMethods listener = new MouseMethods();
-	
-	
-	public DisplayController(TaskSettingView settingView, DisplayTaskPanel displayTaskPanel){
-		this.settingView=settingView;
-		this.displayTaskPanel=displayTaskPanel;
+
+	public DisplayController(TaskSettingView settingView,
+			DisplayTaskPanel displayTaskPanel) {
+		this.settingView = settingView;
+		this.displayTaskPanel = displayTaskPanel;
 		displayTaskPanel.setController(this);
 		displayTaskPanel.addMouseListener(this);
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Entered action preformed");
-		
-		if(e.getSource() instanceof DisplayModel){ 
-			DisplayModel model = (DisplayModel) e.getSource();
-		
-			if(e.getSource() instanceof JComponent){
-				if(e.getActionCommand().equals("editTask")){
-					System.out.println("Pressed editbutton");
-					
+
+		if (e.getSource() instanceof JComponent) {
+			if (e.getActionCommand().equals("editTask")) {
+				System.out.println("Pressed editbutton");
+
+				if (e.getSource() instanceof DisplayModel) {
+					DisplayModel model = (DisplayModel) e.getSource();
 					EditModel editModel = new EditModel();
-					EditTaskPanel editTaskPanel = new EditTaskPanel(model.getTaskModel());
+					EditTaskPanel editTaskPanel = new EditTaskPanel(
+							model.getTaskModel());
 					settingView.removeAll();
 					settingView.add(editTaskPanel);
 					settingView.updateView();
 				}
 			}
 		}
-		
-		
-//		if(e.getSource() instanceof JComponent){
-//			if(e.getActionCommand.equals("editTask")){
-//				editView.setVisible(true);
-//			}
-				
+
+		// if(e.getSource() instanceof JComponent){
+		// if(e.getActionCommand.equals("editTask")){
+		// editView.setVisible(true);
+		// }
+
 	}
-	
-//	private class MouseMethods implements MouseListener{
 
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			if(e.getSource() instanceof DisplayTaskPanel){
-				System.out.println("Clicked Panel");
-				
-			}
-		}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() instanceof DisplayTaskPanel) {
+			System.out.println("Clicked Panel");
 
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			displayTaskPanel.setBackground(Color.blue);
-			
 		}
+	}
 
-		@Override
-		public void mouseExited(MouseEvent e) {
-			displayTaskPanel.setBackground(Color.white);			
-		}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		displayTaskPanel.setBackground(Color.blue);
 
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+	}
 
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		
-//	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		displayTaskPanel.setBackground(Color.white);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
