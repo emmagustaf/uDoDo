5d80817a43e5b3addfe85a966392cbb0b7a7a287
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-05-13 11:23:04 +0200

    added setController in EditTaskPanel and implemented a bit of the actionlistener in EditController. Not finished

--- src/Model/EditModel.java
+++ src/Model/EditModel.java
@@ -9,5 +9,7 @@ public class EditModel {
 	public EditModel(){
 		
 	}
+	
+	 
 
 }
--- src/controller/EditController.java
+++ src/controller/EditController.java
@@ -4,17 +4,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import model.CategoryModel;
-
+import model.*;
 
 public class EditController implements ActionListener{
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {		
-//		if(e.getSource() instanceof EditModel){
-//			EditModel model = (EditModel) e.getSource();
-//			if()
-//		}
-
+	public void actionPerformed(ActionEvent e) {	
+		if(e.getActionCommand().equals("cancel")){
+			if(e.getSource() instanceof EditModel){
+				EditModel model = (EditModel) e.getSource();
+				
+			}
+			
+		}else if(e.getActionCommand().equals("save")){
+			if(e.getSource() instanceof EditModel){
+				EditModel model = (EditModel) e.getSource();
+				
+			}
+		}
+		
+		
 			
 	}
+	
+	
 }
