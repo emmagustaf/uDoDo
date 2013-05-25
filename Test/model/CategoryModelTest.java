ad320618b480eb2c3418a6e96c72c0cbd503715f
materne <hanna.materne@live.se>
2013-05-25 22:20:17 +0200

    a few changes

--- Test/model/CategoryModelTest.java
+++ Test/model/CategoryModelTest.java
@@ -2,20 +2,27 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class CategoryModelTest {
 	
-	@Test
+/*	@Test
 	public void getCatTitleTest(){
 		CategoryModel categoryModel = new CategoryModel(null);
 		
-	}
+
+		assertEquals(categoryModel == categoryModel.getCatTitle());	
+		
+	}*/
 	
 	@Test
 	public void getTaskListTest(){
 		ListModel listModel = new ListModel();
-		
-		
+		CategoryModel categoryModel = new CategoryModel(null);
+		assertTrue(listModel == categoryModel.getTaskList());
 	}
+	
 }
+
