1295dd5bb91c0126539ac369cb98198620f245f3
materne <hanna.materne@live.se>
2013-05-25 14:32:39 +0200

    Started making the taskmodeltest

--- Test/model/TaskModelTest.java
+++ Test/model/TaskModelTest.java
@@ -7,8 +7,46 @@ import org.junit.Test;
 public class TaskModelTest {
 
 	@Test
-	public void test() {
+/*	public void changePriorityTest() {
 		fail("Not yet implemented");
 	}
+	
+	public void changeStateTest(){
+		fail("NOOOOT YEEET IMPLEMENTED");
+	}
 
+	public boolean getStateTest(){
+		return true;
+	}*/
+	
+	public void getTitleTest(){
+		TaskModel taskModel = new TaskModel(null);
+		taskModel.setTitle("test");
+		assertTrue(taskModel.getTitle() == "test");
+	}
+	
+	public void getDescriptionTest(){
+		TaskModel taskModel = new TaskModel(null);
+		taskModel.setDescription("test");
+		assertTrue(taskModel.getDescription() == "test");
+	}
+	
+	public void setTitleTest(){
+		TaskModel taskModel = new TaskModel(null);
+		taskModel.setTitle("test");
+		assertTrue(taskModel.getTitle() == "test");
+	}
+	
+	public void setDescriptionTest(){
+		TaskModel taskModel = new TaskModel(null);
+		taskModel.setDescription("test");
+		assertTrue(taskModel.getDescription() == "test");
+	}
+	
+	public void getDeadLineTest(){
+		TaskModel taskModel = new TaskModel(null);
+		
+	}
+	
+	
 }
