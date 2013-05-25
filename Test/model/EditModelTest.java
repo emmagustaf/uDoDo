8dc50373f80003ebf807ec6c2b01c7c6017207a3
materne <hanna.materne@live.se>
2013-05-25 10:11:27 +0200

    added testclasses for all the models

---
+++ Test/model/EditModelTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EditModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
