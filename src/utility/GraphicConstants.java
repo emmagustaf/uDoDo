33dfc1a3c490a69e750d21ecdf956619e724a9d2
materne <hanna.materne@live.se>
2013-05-25 09:53:21 +0200

    Changed some colours for the displaypanel

--- src/utility/GraphicConstants.java
+++ src/utility/GraphicConstants.java
@@ -6,17 +6,18 @@ import java.awt.Font;
 public class GraphicConstants {
 	
 	public final static Font REGULARFONT = (new Font("Tahoma", Font.PLAIN, 12));
-	public final static Font REGULARBOLDFONT = (new Font("Tahoma", Font.BOLD, 12));
-	public final static Font SMALLHEADINGFONT = (new Font("Tahoma", Font.BOLD, 18));
+	public final static Font REGULARBOLDFONT = (new Font("Tahoma", Font.BOLD,
+			12));
+	public final static Font SMALLHEADINGFONT = (new Font("Tahoma", Font.BOLD,
+			18));
 	public final static Font HEADINGFONT = (new Font("Tahoma", Font.BOLD, 20));
-	
+
 	public final static Color BACKGROUND = (new Color(67, 67, 67));
 	public final static Color PURPLEHAZE = (new Color(125, 113, 128));
 	public final static Color FOREGROUND = Color.WHITE;
-	
-	
-	public final static Color BUTTONPRESSEDFOREGROUND = (new Color(67,67,67));
+
+	public final static Color BUTTONPRESSEDFOREGROUND = (new Color(67, 67, 67));
 	public final static Color BUTTONPRESSED = Color.WHITE;
-	public final static Color BUTTONHOVER = (new Color(0,159,227));
+	public final static Color BUTTONHOVER = (new Color(0, 159, 227));
 
 }
