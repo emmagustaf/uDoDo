ecf410bcb975eb84dba785b1c066458dfe797f3d
materne <hanna.materne@live.se>
2013-05-25 18:05:04 +0200

    deleted editmodel and editmodeltest, and changed some classes due to the deletations

--- Test/model/DisplayModelTest.java
+++ Test/model/DisplayModelTest.java
@@ -10,29 +10,34 @@ public class DisplayModelTest {
 	public void getTaskTitleTest() {
 		TaskModel taskModel = new TaskModel(null);
 		taskModel.setTitle("test");
-		assertTrue(taskModel.getTitle() == "test");
+		assertEquals("test", taskModel.getTitle());
 	}
 	
+	@Test
 	public void setTaskTitleTest(){
 		TaskModel taskModel = new TaskModel("testing");
 		taskModel.setTitle("test");
-		assertTrue(taskModel.getTitle() == "test");
+		assertEquals("test", taskModel.getTitle());
 	}
 	
+	@Test
 	public void getTaskDescriptionTest(){
 		TaskModel taskModel = new TaskModel(null);
 		taskModel.setDescription("test");
-		assertTrue(taskModel.getDescription() == "test");
+		assertEquals("test", taskModel.getDescription());
 	}
 	
+	@Test
 	public void setTaskDescriptionTest(){
 		TaskModel taskModel = new TaskModel(null);
 		taskModel.setDescription("test");
-		assertTrue(taskModel.getDescription() == "test");
+		assertEquals("test", taskModel.getDescription());
 	}
 
+	@Test
 	public void getTaskModelTest(){
 		TaskModel taskModel = new TaskModel(null);
+		
 		
 	}
 }
