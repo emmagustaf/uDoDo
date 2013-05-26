6b2c63f448385a928e0ed142f7870b19ab73d45c
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-05-26 04:28:41 +0200

    fixed with save

--- src/utility/FileToProgram.java
+++ src/utility/FileToProgram.java
@@ -8,38 +8,17 @@ import model.*;
 import controller.*;
 public class FileToProgram {
 	
-	public static void SavedToList(ListView listView, CategoryListView catListView){
+	public static void SavedToList(ListView listView, CategoryListView catListView, TaskSettingView taskSettingView){
 	
 		Read.readFromFile();
-		CategoryModel catModel;
-		TaskSettingView taskSettingView = new TaskSettingView();
+
 		
 		for(int i=0; i<AllTaskListModel.getInstance().size(); i++){
 			TaskPanel panel= new TaskPanel(AllTaskListModel.getInstance().get(i));
 			TaskController taskController = new TaskController(listView, panel, taskSettingView, catListView);
-			
-			DisplayModel dispModel = new DisplayModel(panel.getModel());
-			DisplayTaskPanel dispPanel = new DisplayTaskPanel(dispModel, panel.getModel());
-			taskSettingView.add(dispPanel);
-			DisplayController dispController = new DisplayController(listView, taskSettingView, dispPanel);
-						
-			
-			dispPanel.setController(dispController);
 			panel.setController(taskController);
-
-			
-//			catModel=panel.getModel().getCategory();
-//			AddedCategoryPanel catPanel = new AddedCategoryPanel(catModel);
-//			
-			//CategoryPanelController catController = new CategoryPanelController(taskSettingView, catPanel, catListView, listView);
-		
-//			catPanel.setController(catController);
-//			
-//			catListView.panel.add(catPanel);
-			
+				
 			listView.panelInScroll.add(panel);
-			
-			listView.updateView();
 			
 		}
 		
