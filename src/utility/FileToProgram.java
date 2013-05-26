1eccaf0963803ba78b0ae55853bd4d4cc6e0ee21
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2013-05-26 02:31:28 +0200

    changed method to void

--- src/utility/FileToProgram.java
+++ src/utility/FileToProgram.java
@@ -2,16 +2,28 @@ package utility;
 
 import model.AllTaskListModel;
 import view.*;
-
+import model.*;
+import controller.*;
 public class FileToProgram {
-	private ListView listView;
-	public void TaskToList(ListView listView){
-		this.listView=listView;
+	
+	public static void SavedToList(ListView listView, CategoryListView catListView){
+	
 		Read.readFromFile();
+		CategoryModel catModel;
+		//TaskController taskController = new TaskController();
 		for(int i=0; i<AllTaskListModel.getInstance().size(); i++){
 			TaskPanel panel= new TaskPanel(AllTaskListModel.getInstance().get(i));
+			
+			catModel=panel.getModel().getCategory();
+			AddedCategoryPanel catPanel = new AddedCategoryPanel(catModel);
+			
+			//panel.setController(taskController);
+			
+			catListView.panel.add(catPanel);
 			listView.panelInScroll.add(panel);
+			
 			listView.updateView();
 		}
+		
 	}
 }
