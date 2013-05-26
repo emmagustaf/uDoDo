61a5e4f09bb04c58ac8823096153ac70cfdd7a82
materne <hanna.materne@live.se>
2013-05-26 23:18:01 +0200

    cleaned code in editcontroller

--- src/controller/EditController.java
+++ src/controller/EditController.java
@@ -12,8 +12,8 @@ import calendar.JDateTextField;
 import model.*;
 import view.*;
 
-public class EditController implements ActionListener{
-	
+public class EditController implements ActionListener {
+
 	private ListView listView;
 	private TaskSettingView settingView;
 	private EditTaskPanel editTaskPanel;
@@ -21,64 +21,69 @@ public class EditController implements ActionListener{
 	private DisplayController displayController;
 	private Calendar calendar;
 	private JDateTextField text = new JDateTextField();
-	
-	public EditController(ListView listView, TaskSettingView settingView, EditTaskPanel editTaskPanel){
+
+	public EditController(ListView listView, TaskSettingView settingView,
+			EditTaskPanel editTaskPanel) {
 		this.listView = listView;
 		this.settingView = settingView;
-		this.editTaskPanel=editTaskPanel;
+		this.editTaskPanel = editTaskPanel;
 
 		editTaskPanel.setController(this);
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() instanceof JButton){
-			
-//			DisplayTaskPanel panel = (DisplayTaskPanel)((JButton) e.getSource()).getParent();
-//			TaskModel model = panel.getTaskModel();
-			
-			//Cancels editing
-			if(e.getActionCommand().equals("cancel")){
+		if (e.getSource() instanceof JButton) {
+
+			// DisplayTaskPanel panel = (DisplayTaskPanel)((JButton)
+			// e.getSource()).getParent();
+			// TaskModel model = panel.getTaskModel();
+
+			// Cancels editing
+			if (e.getActionCommand().equals("cancel")) {
 				toDisplayTask();
 				System.out.println("Entered action commant in cancel.");
 				settingView.updateView();
 				listView.updateView();
-				
-			//Saves all updates in edittaskpanel
-			}else if(e.getActionCommand().equals("save")){
+
+				// Saves all updates in edittaskpanel
+			} else if (e.getActionCommand().equals("save")) {
 				setDate();
 				saveChanges();
 				settingView.updateView();
 				listView.updatePanels();
 				listView.updateView();
-				
-			// Enables the calendar pop-up
-			}else if(e.getActionCommand().equals("calendar")){
-				
+
+				// Enables the calendar pop-up
+			} else if (e.getActionCommand().equals("calendar")) {
+
 				editTaskPanel.getCalendar().setVisible(true);
 				editTaskPanel.getCalendar().setEnabled(true);
-				
-				//Updates the deadlinelabel in edittaskpanel before saving to all panels
-				if(editTaskPanel.getCalendar().isOkPressed()){
-					editTaskPanel.deadlineLabel.setText(editTaskPanel.getJDateTextField());
+
+				// Updates the deadlinelabel in edittaskpanel before saving to
+				// all panels
+				if (editTaskPanel.getCalendar().isOkPressed()) {
+					editTaskPanel.deadlineLabel.setText(editTaskPanel
+							.getJDateTextField());
 				}
-				
-				
+
 			}
 			settingView.updateView();
 		}
-		
+
 	}
-	
+
 	/**
 	 * Returns the user to displayTaskPanel after canceling
 	 */
-	private void toDisplayTask(){
+	private void toDisplayTask() {
 		settingView.panelInScroll.removeAll();
 		DisplayModel model = new DisplayModel(editTaskPanel.getTaskModel());
-		displayTaskPanel = new DisplayTaskPanel(model, editTaskPanel.getTaskModel());
-		displayController = new DisplayController(listView, settingView, displayTaskPanel);
+		displayTaskPanel = new DisplayTaskPanel(model,
+				editTaskPanel.getTaskModel());
+		displayController = new DisplayController(listView, settingView,
+				displayTaskPanel);
 		settingView.panelInScroll.add(displayTaskPanel);
 	}
 	
